db85b3ad4e4330ba50fa169b08c1198504a37d5b
ousama lasri <oussama.lasri.2k@gmail.com>
2026-01-27 16:46:10 +0100

    feat: add SequenceGenerator service for order IDs using MongoDB

---
+++ src/main/java/com/oussma/Order/service/SequenceGenerator.java
@@ -0,0 +1,33 @@
+package com.oussma.Order.service;
+
+import static org.springframework.data.mongodb.core.query.Criteria.where;
+import static org.springframework.data.mongodb.core.FindAndModifyOptions.options;
+import static org.springframework.data.mongodb.core.query.Query.query;
+
+import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.data.mongodb.core.query.Update;
+import org.springframework.stereotype.Service;
+
+import com.oussma.Order.entity.Sequence;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class SequenceGenerator {
+
+    private final MongoOperations mongoOperations;
+
+    public int generateNextOrderId(){
+        Sequence counter = mongoOperations.findAndModify(
+                query(where("_id").is("sequence")),
+                new Update().inc("sequence", 1),
+                options().returnNew(true).upsert(true),
+                Sequence.class);
+        return counter.getSequence();
+
+    }
+
+
+
+}
